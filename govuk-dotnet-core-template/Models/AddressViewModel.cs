aa966735577c9d3a3607010420ce61e1de1424f6
MichaelStevenson2207 <wubblyjuggly@gmail.com>
2022-11-02 15:22:54 +0000

    Fixing code smells

--- govuk-dotnet-core-template/Models/AddressViewModel.cs
+++ govuk-dotnet-core-template/Models/AddressViewModel.cs
@@ -8,17 +8,17 @@ namespace govuk_dotnet_core_template.Models
         public string? SearchPostCode { get; set; }
 
         [Required(ErrorMessage = "Enter address line 1")]
-        public string Address1 { get; set; }
+        public string? Address1 { get; set; }
 
         public string? Address2 { get; set; }
 
         public string? Address3 { get; set; }
 
         [Required(ErrorMessage = "Enter town or city")]
-        public string TownCity { get; set; }
+        public string? TownCity { get; set; }
 
         [Required(ErrorMessage = "Enter post code")]
-        public string PostCode { get; set; }
+        public string? PostCode { get; set; }
 
         public string? Country { get; set; }
     }
